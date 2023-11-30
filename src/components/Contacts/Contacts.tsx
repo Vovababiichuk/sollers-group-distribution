eecdd849cf6a52aa925fc4b95bbbfd9e766836bd
Vovababiichuk <babihuk888@gmail.com>
2023-11-30 14:20:06 +0200

    added react hook form

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,70 +1,68 @@
 // Import necessary libraries and components
 import React, { useRef } from 'react';
-import { Input, InputGroup, InputLeftElement, Stack, Wrap, WrapItem } from '@chakra-ui/react';
-import { Textarea } from '@chakra-ui/react';
+import { Input, Box, Stack, Textarea } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 // @ts-expect-error This line intentionally triggers a TypeScript error due to specific library constraints
 import { fadeIn } from '../../../variants'; // Assuming this is a valid import
 import emailjs from '@emailjs/browser';
-import { PhoneIcon } from 'lucide-react';
-import { useToast } from '@chakra-ui/react';
 
 import styles from './Contacts.module.scss';
-import { ButtonMain } from '../ButtonMain/ButtonMain';
+// import { ButtonMain } from '../ButtonMain/ButtonMain';
 
-// Define a type for toast status
-type ToastStatus = 'success' | 'error' | 'warning' | 'info';
+import { useForm } from 'react-hook-form';
 
-// Define the Contacts functional component
+interface FormData {
+  name: string;
+  position: string;
+  phone: string | number;
+  email: string;
+  message: string;
+}
+
+const ERRORS_MESSAGE = {
+  name: 'Name is required field!',
+  position: 'Position is required field!',
+  phone: 'Phone is required field!',
+  email: 'Email is required field!',
+  message: 'Message is required field!',
+};
+
+const REG_EX = {
+  email: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
+};
+
 export const Contacts: React.FC = () => {
-  const toast = useToast(); // Use Chakra UI toast hook
-  const form = useRef<HTMLFormElement>(null); // Reference to the form element
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormData>({
+    mode: 'onChange',
+  });
 
-  // Function to show toast based on status
-  const showToast = (status: ToastStatus) => {
-    toast({
-      title: `${status} toast`,
-      status: status,
-      isClosable: true,
-    });
-  };
+  const form = useRef<HTMLFormElement | null>(null);
 
-  // Function to handle form submission
-  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    if (form.current && form.current instanceof HTMLFormElement) {
-      const formData = new FormData(form.current);
-      const user_name = formData.get('user_name') as string;
-      const user_email = formData.get('user_email') as string;
-      const message = formData.get('message') as string;
-
-      if (!user_name || !user_email || !message) {
-        showToast('error');
-        console.error('Please fill in all required fields.');
-        return;
-      }
-
-      // Use emailjs to send the form data
-      emailjs
-        .sendForm('service_libgagm', 'template_erdrx1n', form.current, 'SLEgXy96TDHoBfFP9')
-        .then(
-          (result) => {
-            console.log(result.text);
-            console.log('Email sent successfully!');
-            showToast('success'); // Show success toast
-          },
-          (error) => {
-            console.log(error.text);
-            showToast('error'); // Show error toast
-          },
+  const sendEmail = async () => {
+    try {
+      if (form.current) {
+        const result = await emailjs.sendForm(
+          'service_libgagm',
+          'template_erdrx1n',
+          form.current,
+          'SLEgXy96TDHoBfFP9'
         );
-    } else {
-      console.error('Form reference is null or not an HTMLFormElement.');
+    
+        console.log(result.text);
+        console.log('Email відправлено успішно!');
+        reset();
+      }
+    } catch (error) {
+      console.error('Email не відправлено!');
     }
   };
+  
 
-  // Render the component
   return (
     <div id="contact" className="text-white bg-color_black-section2 h-[800px] pt-40 relative">
       <div className={styles.container}>
@@ -92,61 +90,150 @@ export const Contacts: React.FC = () => {
               </span>
               <h2 className="text-3xl mb-8 font-bold">Write to us to get the best!</h2>
 
-              {/* Form for user input */}
-              <form ref={form} onSubmit={sendEmail}>
-                <Stack spacing={3}>
-                  <Input
-                    required
-                    type="text"
-                    minLength={3}
-                    name="user_name"
-                    variant="outline"
-                    placeholder="Your Name"
-                  />
-                  <Input
-                    required
-                    minLength={3}
-                    type="text"
-                    name="user_position"
-                    variant="outline"
-                    placeholder="Your Position"
-                  />
-                  <InputGroup>
-                    <InputLeftElement pointerEvents="none">
-                      <PhoneIcon size={20} color="white" />
-                    </InputLeftElement>
-                    <Input
-                      required
-                      minLength={10}
-                      type="tel"
-                      inputMode='numeric'
-                      pattern="[0-9]*"
-                      name="user_phone"
-                      placeholder="Phone number"
-                    />
-                  </InputGroup>
-                  <Input
-                    required
-                    type="email"
-                    name="user_email"
-                    variant="outline"
-                    placeholder="Your Mail"
-                  />
-                  <Textarea required name="message" placeholder="Write your comment here" />
-
-                  <motion.div
-                    variants={fadeIn('up', 0.1)}
-                    initial="hidden"
-                    whileInView={'show'}
-                    viewport={{ once: false, amount: 0.3 }}>
-                    {/* Buttons to trigger toasts */}
-                    <Wrap paddingTop={'10px'} margin={'auto'} justify={'center'} color={'black'}>
-                      <WrapItem>
-                        <ButtonMain name="Send" blackBtn={false} />
-                      </WrapItem>
-                    </Wrap>
-                  </motion.div>
-                </Stack>
+              {/* Form */}
+              <form ref={form} onSubmit={handleSubmit(sendEmail)} >
+                <div className="flex flex-col gap-2">
+                  {/* Name */}
+                  <div>
+                    <Stack spacing={3}>
+                      <Input
+                        {...register('name', {
+                          required: ERRORS_MESSAGE.name,
+                        })}
+                        type="text"
+                        name="name"
+                        variant="outline"
+                        placeholder="Name"
+                        focusBorderColor="#32b4ff"
+                      />
+                    </Stack>
+                  </div>
+                  {/* errors */}
+                  <div>
+                    {errors.name && (
+                      <span className="text-red-600 ml-2">{errors.name.message}</span>
+                    )}
+                  </div>
+                  {/* Position */}
+                  <div>
+                    <Stack spacing={3}>
+                      <Input
+                        {...register('position', {
+                          required: ERRORS_MESSAGE.position,
+                        })}
+                        type="text"
+                        name="position"
+                        variant="outline"
+                        placeholder="Position"
+                        focusBorderColor="#32b4ff"
+                      />
+                    </Stack>
+                  </div>
+                  {/* errors */}
+                  <div>
+                    {errors.position && (
+                      <span className="text-red-600 ml-2">{errors.position.message}</span>
+                    )}
+                  </div>
+                  {/* Phone */}
+                  <div>
+                    <Stack spacing={3}>
+                      <Input
+                        {...register('phone', {
+                          required: ERRORS_MESSAGE.phone,
+                        })}
+                        type="text"
+                        name="phone"
+                        variant="outline"
+                        placeholder="Phone"
+                        focusBorderColor="#32b4ff"
+                      />
+                    </Stack>
+                  </div>
+                  {/* errors */}
+                  <div>
+                    {errors.phone && (
+                      <span className="text-red-600 ml-2">{errors.phone.message}</span>
+                    )}
+                  </div>
+                  {/* Email */}
+                  <div>
+                    <Stack spacing={3}>
+                      <Input
+                        {...register('email', {
+                          required: ERRORS_MESSAGE.email,
+                          pattern: {
+                            value: REG_EX.email,
+                            message: 'Please enter a valid email address!',
+                          },
+                        })}
+                        type="text"
+                        name="email"
+                        variant="outline"
+                        placeholder="Email"
+                        focusBorderColor="#32b4ff"
+                      />
+                    </Stack>
+                  </div>
+                  {/* errors */}
+                  <div>
+                    {errors.email && (
+                      <span className="text-red-600 ml-2">{errors.email.message}</span>
+                    )}
+                  </div>
+                  {/* Message */}
+                  <div>
+                    <Stack spacing={3}>
+                      
+                      <Textarea
+                        {...register('message', {
+                          required: ERRORS_MESSAGE.message,
+                        })}
+                        name="message"
+                        variant="outline"
+                        placeholder="Comments"
+                        focusBorderColor="#32b4ff"
+                      />
+                    </Stack>
+                  </div>
+                  {/* errors */}
+                  <div>
+                    {errors.message && (
+                      <span className="text-red-600 ml-2">{errors.message.message}</span>
+                    )}
+                  </div>
+                  {/* Button */}
+                  <div className="m-auto">
+                    <Box
+                      type="submit"
+                      as="button"
+                      margin={'10px 0'}
+                      height="32px"
+                      lineHeight="1.2"
+                      transition="all 0.2s cubic-bezier(.08,.52,.52,1)"
+                      border="1px"
+                      px="18px"
+                      borderRadius="6px"
+                      fontSize="14px"
+                      fontWeight="semibold"
+                      bg="#ea4e85"
+                      borderColor="#ea4e85"
+                      color="#fff"
+                      _hover={{ bg: '#ff8eb5' }}
+                      _active={{
+                        bg: '#ff8eb5',
+                        transform: 'scale(0.98)',
+                        borderColor: '#ff8eb5',
+                      }}
+                      _focus={{
+                        boxShadow:
+                          '0 0 1px 2px rgba(255, 88, 130, 0.75), 0 1px 1px rgba(0, 0, 0, .15)',
+                      }}>
+                      Send
+                    </Box>
+                  </div>
+                  <div></div>
+                </div>
               </form>
             </motion.div>
           </div>
