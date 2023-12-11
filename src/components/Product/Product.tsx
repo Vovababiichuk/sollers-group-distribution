1b48f65be861d16b370509ad2652ad9706a4e699
Vovababiichuk <babihuk888@gmail.com>
2023-12-11 20:00:22 +0200

    fix bag

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -49,7 +49,7 @@ export const Product: React.FC = () => {
           </div>
         </motion.div>
 
-        <div className="flex items-center gap-[10px] mt-[-30px]">
+        <div className="flex items-center gap-[100px] max-my-600:gap-[0] mt-[-30px]">
           <motion.div
             variants={fadeIn('right', 0.2)}
             initial="hidden"
